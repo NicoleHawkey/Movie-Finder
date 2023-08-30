cb9f5533a580de9d1cef3704073160233a6cfa30
Nicole Hawkey <n.laggner@gmx.at>
2023-08-31 10:15:01 +1200

    formatDate

--- src/components/Moviecard/MovieCard.js
+++ src/components/Moviecard/MovieCard.js
@@ -7,6 +7,15 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 const MovieCard = ({ movies }) => {
     if (!movies || movies.length === 0) return null;
 
+    function formatDate(dateString) {
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const date = new Date(dateString);
+        const monthName = months[date.getMonth()];
+        const day = date.getDay();
+        const year = date.getFullYear();
+        return `${monthName} ${day}, ${year}`
+    }
+
     return ( 
         <div className="movie-body">
                 <ul>
@@ -19,7 +28,7 @@ const MovieCard = ({ movies }) => {
                         <div className="movie-text">
                             <h2>{movie.title}</h2>
                             <p><FontAwesomeIcon icon={faStar} />{Number(movie.vote_average).toFixed(1)}</p>
-                            <h3>{movie.release_date}</h3>
+                            <h3>{formatDate(movie.release_date)}</h3>
                             <p>{movie.overview.slice(0, 250)}{movie.overview.length > 250 ? "..." : ""}</p>
                         </div>
                     </li>
